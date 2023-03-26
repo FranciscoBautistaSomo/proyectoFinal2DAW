645bd2259e19567293a7424099150f0462696a09
FranBau <franciscoalberto.bautistasom@viewnext.com>
2023-03-26 21:20:03 +0200

    Proyecto configurado
    Proyecto configurado con la primera versión

---
+++ metaversoWebShop/src/main/java/com/metaverso/webshop/MetaversoWebShopApplication.java
@@ -0,0 +1,13 @@
+package com.metaverso.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaversoWebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaversoWebShopApplication.class, args);
+	}
+
+}

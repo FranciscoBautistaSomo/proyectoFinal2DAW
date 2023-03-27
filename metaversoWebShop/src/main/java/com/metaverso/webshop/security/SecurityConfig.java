9294661057ed570d0afa1b270b14e9b68bf5beb2
FranBau <franciscoalberto.bautistasom@viewnext.com>
2023-03-27 21:18:53 +0200

    Mas configuración de seguridad
    Ultima actualización

--- metaversoWebShop/src/main/java/com/metaverso/webshop/security/SecurityConfig.java
+++ metaversoWebShop/src/main/java/com/metaverso/webshop/security/SecurityConfig.java
@@ -1,24 +1,41 @@
 package com.metaverso.webshop.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-		@Bean
-		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			http
-						.csrf().disable()
-						.authorizeRequests()
-						.anyRequest().authenticated()
-						.and()
-						.httpBasic();
-			return http.build();
-		}
-		
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET).permitAll().anyRequest().authenticated()
+				.and().httpBasic();
+		return http.build();
+	}
+
+//	@Bean
+//		public UserDetailsService users() {
+//			UserDetails admin = User.builder()
+//								.username("admin")
+//								.password("admin")
+//								.roles("ADMIN")
+//								.build();
+//				UserDetails user = User.builder()
+//								.username("user")
+//								.password("1234")
+//								.roles("USER")
+//								.build();
+//				
+//				return new InMemoryUserDetailsManager(admin, user);
+//		}
+
 }

1e6067df9a9a91063cdb4ecce81412f3a5c8fef9
FranBau <franciscoalberto.bautistasom@viewnext.com>
2023-03-29 21:02:43 +0200

    Añadido nuevos modelos
    Se crea el modelo Roles y se modifica Usuarios

--- metaversoWebShop/src/main/java/com/metaverso/webshop/model/Usuario.java
+++ metaversoWebShop/src/main/java/com/metaverso/webshop/model/Usuario.java
@@ -1,6 +1,20 @@
 package com.metaverso.webshop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+import com.metaverso.webshop.model.Role;
 
 import lombok.Data;
 
@@ -12,7 +26,12 @@ public class Usuario  {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String username;
+    private String nombre;
     private String password;
+    
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "usuario_roles", joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
 	
 }

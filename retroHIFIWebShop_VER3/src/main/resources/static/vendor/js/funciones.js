45fd52980a23c2e181ef559b1f892fa28baf024b
FranBau <franciscoalberto.bautistasom@viewnext.com>
2023-05-22 12:39:04 +0200

    Actualización proyecto
    Ultima actualización, filtro tablas y paginacion

--- retroHIFIWebShop_VER3/src/main/resources/static/vendor/js/funciones.js
+++ retroHIFIWebShop_VER3/src/main/resources/static/vendor/js/funciones.js
@@ -125,3 +125,36 @@ function cerrar() {
 	}
 }
 
+var seleccion = 0;
+
+$("input[name='filtro']" ).on('change', function () {
+    seleccion = $(this).val();
+    /*alert(seleccion);*/
+});
+
+
+
+function filtro() {
+    // Accede al texto de entrada y a varios elementos del DOM
+    let valor = document.getElementById("searchInput").value.toUpperCase();
+    let nombres = document.getElementById("names");
+    let filas = nombres.getElementsByTagName("tr");
+    
+    console.log(valor);
+  
+    // Iteramos el arreglo de filas
+    for(let i = 0; i < filas.length; i++){
+      // Por cada fila se obtiene la referencia a la columna lenguaje
+      let columnaLenguaje = filas[i].getElementsByTagName("td")[seleccion];
+      // Se extrae el texto de la columna lenguaje
+      let lenguaje = columnaLenguaje.textContent;
+      // Se muestra u oculta la fila si la entrada de texto
+      // coincide con el texto de la columna lenguaje
+      filas[i].style.display = lenguaje.toUpperCase().indexOf(valor) > -1 ? "" : "none";
+    }
+  }
+  
+  // Se agrega un escuchador de eventos a la entrada de texto
+  // para activar la función de filtrado cada vez que el usuario
+  // ingresa texto en la entrada
+  document.getElementById("searchInput").addEventListener("keyup", filtro);

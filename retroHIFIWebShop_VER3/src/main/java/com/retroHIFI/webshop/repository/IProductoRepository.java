a07a6f095413333542eeefdcb65d7d2ff9cc2397
FranciscoBautistaSomo <franciscoalberto.bautista.fp@iescampanillas.com>
2023-06-18 00:13:26 +0200

    Nuevos ajustes
    Se añaden productos y se cambia barra de navegación

--- retroHIFIWebShop_VER3/src/main/java/com/retroHIFI/webshop/repository/IProductoRepository.java
+++ retroHIFIWebShop_VER3/src/main/java/com/retroHIFI/webshop/repository/IProductoRepository.java
@@ -16,37 +16,37 @@ import java.util.*;
 @Repository
 public interface IProductoRepository  extends JpaRepository<Producto, Integer>{
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.categoria_id_categoria = 1 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.categoria_id_categoria = 1 AND p.enabled = 1", nativeQuery=true)
 	List<Producto> mostrarAudio();
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.categoria_id_categoria = 1 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.categoria_id_categoria = 1 AND p.enabled = 1", nativeQuery=true)
 	Page<Producto> mostrarAudios(Pageable pageable);	
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.categoria_id_categoria = 2 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.categoria_id_categoria = 2 AND p.enabled = 1", nativeQuery=true)
 	List<Producto> mostrarVideo();
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.categoria_id_categoria = 2 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.categoria_id_categoria = 2 AND p.enabled = 1", nativeQuery=true)
 	Page<Producto> mostrarVideos(Pageable pageable);
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.categoria_id_categoria = 3 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.categoria_id_categoria = 3 AND p.enabled = 1", nativeQuery=true)
 	List<Producto> mostrarSegMano();
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.categoria_id_categoria = 3 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.categoria_id_categoria = 3 AND p.enabled = 1", nativeQuery=true)
 	Page<Producto> mostrarSegManos(Pageable pageable);
 	
-	@Query(value="SELECT nombre FROM Categoria c WHERE c.id_categoria LIKE %:id_categoria%", nativeQuery=true)
+	@Query(value="SELECT nombre FROM categoria c WHERE c.id_categoria LIKE %:id_categoria%", nativeQuery=true)
 	String  getNombreCategoria(@Param("id_categoria") Integer id_categoria);
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.nombre LIKE %:busqueda% AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.nombre LIKE %:busqueda% AND p.enabled = 1", nativeQuery=true)
 	List<Producto> buscador(@Param("busqueda") String busqueda);
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.nombre LIKE %:busqueda% AND p.categoria_id_categoria = 1 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.nombre LIKE %:busqueda% AND p.categoria_id_categoria = 1 AND p.enabled = 1", nativeQuery=true)
 	List<Producto> buscarAudio(@Param("busqueda") String busqueda);
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.nombre LIKE %:busqueda% AND p.categoria_id_categoria = 2 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.nombre LIKE %:busqueda% AND p.categoria_id_categoria = 2 AND p.enabled = 1", nativeQuery=true)
 	List<Producto> buscarVideo(@Param("busqueda") String busqueda);
 	
-	@Query(value="SELECT * FROM Productos p WHERE p.nombre LIKE %:busqueda% AND p.categoria_id_categoria = 3 AND p.enabled = 1", nativeQuery=true)
+	@Query(value="SELECT * FROM productos p WHERE p.nombre LIKE %:busqueda% AND p.categoria_id_categoria = 3 AND p.enabled = 1", nativeQuery=true)
 	List<Producto> buscarSegMano(@Param("busqueda") String busqueda);
 	
 }
